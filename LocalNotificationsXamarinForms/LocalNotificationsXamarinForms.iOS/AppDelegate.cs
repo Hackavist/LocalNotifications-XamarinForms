53d9542a1b71df317202338aa50645052d28346e
Nour El Gafy <nour-elgafy@outlook.com>
2020-06-18 11:27:01 +0200

    Init commit

---
+++ LocalNotificationsXamarinForms/LocalNotificationsXamarinForms.iOS/AppDelegate.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+
+namespace LocalNotificationsXamarinForms.iOS
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the 
+    // User Interface of the application, as well as listening (and optionally responding) to 
+    // application events from iOS.
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        //
+        // This method is invoked when the application has loaded and is ready to run. In this 
+        // method you should instantiate the window, load the UI into it and then make the window
+        // visible.
+        //
+        // You have 17 seconds to return from this method, or iOS will terminate your application.
+        //
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            global::Xamarin.Forms.Forms.Init();
+            LoadApplication(new App());
+            CheckUserNotification();
+            return base.FinishedLaunching(app, options);
+        }
+
+         private static void CheckUserNotification()
+        {
+            if (UIDevice.CurrentDevice.CheckSystemVersion(10, 0))
+            {
+                // Ask the user for permission to get notifications on iOS 10.0+
+                UNUserNotificationCenter.Current.RequestAuthorization(
+                        UNAuthorizationOptions.Alert | UNAuthorizationOptions.Badge | UNAuthorizationOptions.Sound,
+                        (approved, error) => { });
+            }
+            else if (UIDevice.CurrentDevice.CheckSystemVersion(8, 0))
+            {
+                // Ask the user for permission to get notifications on iOS 8.0+
+                var settings = UIUserNotificationSettings.GetSettingsForTypes(
+                        UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound,
+                        new NSSet());
+
+                UIApplication.SharedApplication.RegisterUserNotificationSettings(settings);
+            }
+        }
+    }
+}
---
+++ LocalNotificationsXamarinForms/LocalNotificationsXamarinForms/INotifier.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+using Xamarin.Essentials;
+
+namespace LocalNotificationsXamarinForms
+{
+    public interface INotifierService
+    {
+        /// <summary>
+        /// The Executed Behaviour when the app receives a notification  
+        /// </summary>
+        event EventHandler NotificationReceived;
+
+        /// <summary>
+        /// Executes the platform Specific service initiation code 
+        /// </summary>
+        void Initialize();
+
+        /// <summary>
+        /// Show a local notification now
+        /// </summary>
+        /// <param name="title">Title of the notification</param>
+        /// <param name="body">Body or description of the notification</param>
+        /// <param name="id">Id of the notification</param>
+        void FireANotification(string title, string body, int id = 0);
+
+        /// <summary>
+        /// Show a local notification at a certain date and time 
+        /// </summary>
+        /// <param name="title">Title of the notification</param>
+        /// <param name="body">Body or description of the notification</param>
+        /// <param name="notificationDateTime"> The date and time the notification should be fired </param>
+        /// <param name="id">Id of the notification</param>
+        void FireANotification(string title, string body, DateTime notificationDateTime, int id = 0);
+
+        /// <summary>
+        /// Show a local notification at a certain location 
+        /// </summary>
+        /// <param name="title">Title of the notification</param>
+        /// <param name="body">Body or description of the notification</param>
+        /// <param name="notificationGeolocation"> The Geolocation where the notification should be fired </param>
+        /// <param name="id">Id of the notification</param>
+        void FireANotification(string title, string body, Location notificationGeolocation, int id = 0);
+
+        /// <summary>
+        /// Cancel a local notification
+        /// </summary>
+        /// <param name="id">Id of the notification to cancel</param>
+        void Cancel(int id);
+
+
+        void ReceiveNotification(string title, string message);
+
+    }
+}
